b05cb85c2964a0f18b9e75f50e2f4b3388fdad5f
pomarc <pomarc@gmail.com>
2018-04-23 17:21:42 +0200

    first commit

---
+++ Specialization/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Specialization.Controllers
+{
+    public class SpecializedHomeController : Controller
+    {
+        [Route("Home/ListaCitta")]
+        [HttpGet]
+        public ActionResult Bar()
+        {
+            var c = new List<string>();
+            c.Add("stoccarda");
+            c.Add("stoffredda");
+            c.Add("stoccafisso");
+            
+            return new JsonResult() { JsonRequestBehavior = JsonRequestBehavior.AllowGet,Data=c };
+        }
+
+
+        [Route("Home/ListaCani")]
+        [HttpGet]
+        public ActionResult  Cani()
+        {
+            var c = new List<string>();
+            c.Add("stoccarda");
+            c.Add("stoffredda");
+            c.Add("stoccafisso");
+            c.Add("stoccazzo");
+            return new ContentResult() { Content = c.ToString() }  ;
+        }
+    }
+}
